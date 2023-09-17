f21a3aa5ac339724da04e36c5ab03042d68cd41d
NgoLinhChi <lhcii242@gmail.com>
2023-09-17 23:33:33 +0700

    C#4

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,10 +1,14 @@
-﻿Console.WriteLine ("Nhap so thu nhat");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ("Nhap so thu hai");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Tich cua {0} va {1} là {2}",a,b,a*b);
-Console.WriteLine("Thuong cua {0} va {1} là {2}",a,b,a/b);
-Console.WriteLine("Hieu cua {0} va {1} là {2}",a,b,a-b);
-Console.WriteLine("So du cua {0} va {1} là {2}",a,b,a%b);
-Console.WriteLine("Ngo Linh Chi");
+﻿Console.WriteLine("Ngo Linh Chi");
 Console.WriteLine("2021050894");
+Console.WriteLine("Nhap so thu nhat");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Nhap so thu hai");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b) {
+    Console.WriteLine("{0} bang {1}",a,b);
+}   else {
+    if (a>b){
+    Console.WriteLine("{0} lon hon {1}",a,b);
+}   else 
+    Console.WriteLine("{0} nho hon {1}",a,b);
+}
