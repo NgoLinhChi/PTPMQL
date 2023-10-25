f7539bd7fae90312cccc56399b57ce2d83350043
NgoLinhChi <lhcii242@gmail.com>
2023-10-25 16:20:51 +0700

    'MVCPTB1'

--- Project/FirstWebMVC/Controllers/DemoController.cs
+++ Project/FirstWebMVC/Controllers/DemoController.cs
@@ -10,38 +10,19 @@ namespace FirstWebMVC.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Index(double HSa,double HSb,double HSc)
+        public IActionResult Index(double HSa,double HSb)
         {
-           string phuongtrinh = "Phuong trinh da nhap la: " + HSa + "x^2 + " + HSb + "x +" + HSc + " = 0";
+           string phuongtrinh = "Phuong trinh da nhap la: " + HSa + "x +"  + HSb + " = 0";
            string FormResult;
-           double delta = HSb * HSb + 4 * HSa * HSc;
-           double x1,x2;
            if (HSa ==0) 
            {
-            if (HSb == 0)
-            {
                 FormResult = "Phuong trinh vo nghiem.";
             }
             else
             {
-                FormResult = "Phuong trinh co nghiem x = " + (-HSc / HSb);
+                FormResult = "Phuong trinh co nghiem x = " + (-HSb / HSa);
             }
-           }
-            else if (delta > 0)
-            {
-                x1 = (double)((HSb + Math.Sqrt(delta)) /2 * HSa);
-                x2= (double)((HSb - Math.Sqrt(delta)) /2 * HSa);
-                FormResult = "Phuong trinh co 2 nghiem là x1 = " + x1 + " va x2 = " + x2;
-            }
-            else if (delta == 0)
-            {
-                x1= (-HSb/(2*HSa));
-                FormResult= "Phuong trinh co nghiem kep x1 = x2 = " + x1;
-            }
-            else
-            {
-                FormResult = "Phuong trinh vo nghiem.";
-            }
+           
             ViewBag.debai=phuongtrinh;
             ViewBag.Noti = FormResult;
             return View();
