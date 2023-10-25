bd0c2f2419d76ac62a947cace0423458ee13570c
NgoLinhChi <lhcii242@gmail.com>
2023-10-25 16:00:39 +0700

    'MVCPTB2'

--- Project/FirstWebMVC/Controllers/DemoController.cs
+++ Project/FirstWebMVC/Controllers/DemoController.cs
@@ -10,9 +10,39 @@ namespace FirstWebMVC.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Index(string Fullname, string Email)
+        public IActionResult Index(double HSa,double HSb,double HSc)
         {
-            string FormResult = "Xin chao " + Fullname + " - " + Email;
+           string phuongtrinh = "Phuong trinh da nhap la: " + HSa + "x^2 + " + HSb + "x +" + HSc + " = 0";
+           string FormResult;
+           double delta = HSb * HSb + 4 * HSa * HSc;
+           double x1,x2;
+           if (HSa ==0) 
+           {
+            if (HSb == 0)
+            {
+                FormResult = "Phuong trinh vo nghiem.";
+            }
+            else
+            {
+                FormResult = "Phuong trinh co nghiem x = " + (-HSc / HSb);
+            }
+           }
+            else if (delta > 0)
+            {
+                x1 = (double)((HSb + Math.Sqrt(delta)) /2 * HSa);
+                x2= (double)((HSb - Math.Sqrt(delta)) /2 * HSa);
+                FormResult = "Phuong trinh co 2 nghiem là x1 = " + x1 + " va x2 = " + x2;
+            }
+            else if (delta == 0)
+            {
+                x1= (-HSb/(2*HSa));
+                FormResult= "Phuong trinh co nghiem kep x1 = x2 = " + x1;
+            }
+            else
+            {
+                FormResult = "Phuong trinh vo nghiem.";
+            }
+            ViewBag.debai=phuongtrinh;
             ViewBag.Noti = FormResult;
             return View();
         }
